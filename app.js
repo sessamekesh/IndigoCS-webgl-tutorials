8194fa6fbfb44abac0cef9aaa96f906b8a34c9ef
Kamaron Peterson <kam.is.amazing@gmail.com>
2016-01-29 01:49:28 -0700

    WebGL Tutorial 04 - Loading External Files
    Commit accompanying YouTube video located at
    https://www.youtube.com/watch?v=sM9n73-HiNA

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-var vertexShaderText = 
-[
-'precision mediump float;',
-'',
-'attribute vec3 vertPosition;',
-'attribute vec2 vertTexCoord;',
-'varying vec2 fragTexCoord;',
-'uniform mat4 mWorld;',
-'uniform mat4 mView;',
-'uniform mat4 mProj;',
-'',
-'void main()',
-'{',
-'  fragTexCoord = vertTexCoord;',
-'  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
-'}'
-].join('\n');
-
-var fragmentShaderText =
-[
-'precision mediump float;',
-'',
-'varying vec2 fragTexCoord;',
-'uniform sampler2D sampler;',
-'',
-'void main()',
-'{',
-'  gl_FragColor = texture2D(sampler, fragTexCoord);',
-'}'
-].join('\n');
-
 var gl;
 
 var InitDemo = function () {
+	loadTextResource('/shader.vs.glsl', function (vsErr, vsText) {
+		if (vsErr) {
+			alert('Fatal error getting vertex shader (see console)');
+			console.error(vsErr);
+		} else {
+			loadTextResource('/shader.fs.glsl', function (fsErr, fsText) {
+				if (fsErr) {
+					alert('Fatal error getting fragment shader (see console)');
+					console.error(fsErr);
+				} else {
+					loadJSONResource('/Susan.json', function (modelErr, modelObj) {
+						if (modelErr) {
+							alert('Fatal error getting Susan model (see console)');
+							console.error(fsErr);
+						} else {
+							loadImage('/SusanTexture.png', function (imgErr, img) {
+								if (imgErr) {
+									alert('Fatal error getting Susan texture (see console)');
+									console.error(imgErr);
+								} else { 
+									RunDemo(vsText, fsText, img, modelObj);
+								}
+							});
+						}
+					});
+				}
+			});
+		}
+	});
+};
+
+var RunDemo = function (vertexShaderText, fragmentShaderText, SusanImage, SusanModel) {
 	console.log('This is working');
 
 	var canvas = document.getElementById('game-surface');
@@ -91,107 +92,52 @@ var InitDemo = function () {
 	//
 	// Create buffer
 	//
-	var boxVertices = 
-	[ // X, Y, Z           U, V
-		// Top
-		-1.0, 1.0, -1.0,   0, 0,
-		-1.0, 1.0, 1.0,    0, 1,
-		1.0, 1.0, 1.0,     1, 1,
-		1.0, 1.0, -1.0,    1, 0,
+	var susanVertices = SusanModel.meshes[0].vertices;
+	var susanIndices = [].concat.apply([], SusanModel.meshes[0].faces);
+	var susanTexCoords = SusanModel.meshes[0].texturecoords[0];
 
-		// Left
-		-1.0, 1.0, 1.0,    0, 0,
-		-1.0, -1.0, 1.0,   1, 0,
-		-1.0, -1.0, -1.0,  1, 1,
-		-1.0, 1.0, -1.0,   0, 1,
+	var susanPosVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, susanPosVertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(susanVertices), gl.STATIC_DRAW);
 
-		// Right
-		1.0, 1.0, 1.0,    1, 1,
-		1.0, -1.0, 1.0,   0, 1,
-		1.0, -1.0, -1.0,  0, 0,
-		1.0, 1.0, -1.0,   1, 0,
+	var susanTexCoordVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, susanTexCoordVertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(susanTexCoords), gl.STATIC_DRAW);
 
-		// Front
-		1.0, 1.0, 1.0,    1, 1,
-		1.0, -1.0, 1.0,    1, 0,
-		-1.0, -1.0, 1.0,    0, 0,
-		-1.0, 1.0, 1.0,    0, 1,
+	var susanIndexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, susanIndexBufferObject);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(susanIndices), gl.STATIC_DRAW);
 
-		// Back
-		1.0, 1.0, -1.0,    0, 0,
-		1.0, -1.0, -1.0,    0, 1,
-		-1.0, -1.0, -1.0,    1, 1,
-		-1.0, 1.0, -1.0,    1, 0,
-
-		// Bottom
-		-1.0, -1.0, -1.0,   1, 1,
-		-1.0, -1.0, 1.0,    1, 0,
-		1.0, -1.0, 1.0,     0, 0,
-		1.0, -1.0, -1.0,    0, 1,
-	];
-
-	var boxIndices =
-	[
-		// Top
-		0, 1, 2,
-		0, 2, 3,
-
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
-	];
-
-	var boxVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-
-	var boxIndexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
-
+	gl.bindBuffer(gl.ARRAY_BUFFER, susanPosVertexBufferObject);
 	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-	var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
 	gl.vertexAttribPointer(
 		positionAttribLocation, // Attribute location
 		3, // Number of elements per attribute
 		gl.FLOAT, // Type of elements
 		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
 		0 // Offset from the beginning of a single vertex to this attribute
 	);
+	gl.enableVertexAttribArray(positionAttribLocation);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, susanTexCoordVertexBufferObject);
+	var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
 	gl.vertexAttribPointer(
 		texCoordAttribLocation, // Attribute location
 		2, // Number of elements per attribute
 		gl.FLOAT, // Type of elements
 		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+		2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0
 	);
-
-	gl.enableVertexAttribArray(positionAttribLocation);
 	gl.enableVertexAttribArray(texCoordAttribLocation);
 
 	//
 	// Create texture
 	//
-	var boxTexture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+	var susanTexture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, susanTexture);
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
@@ -199,7 +145,7 @@ var InitDemo = function () {
 	gl.texImage2D(
 		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
 		gl.UNSIGNED_BYTE,
-		document.getElementById('crate-image')
+		SusanImage
 	);
 	gl.bindTexture(gl.TEXTURE_2D, null);
 
@@ -240,10 +186,10 @@ var InitDemo = function () {
 		gl.clearColor(0.75, 0.85, 0.8, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-		gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+		gl.bindTexture(gl.TEXTURE_2D, susanTexture);
 		gl.activeTexture(gl.TEXTURE0);
 
-		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+		gl.drawElements(gl.TRIANGLES, susanIndices.length, gl.UNSIGNED_SHORT, 0);
 
 		requestAnimationFrame(loop);
 	};
